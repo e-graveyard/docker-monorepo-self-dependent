32eeb0c3b223904f2ea47def4c80b81f406f3e06
Caian Ertl <hi@caian.org>
2022-05-12 02:27:17 -0300

    feat: add nodejs sample

---
+++ samples/node/index.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const dayjs = require('dayjs')
+
+const capitalize = s => s && s[0].toUpperCase() + s.slice(1)
+
+const getDate = (daysToAdd = 0) => dayjs().add(daysToAdd, 'days').format('YYYY-MM-DD')
+
+function getCityArg() {
+    const fallback = 'manhattan'
+
+    const city = (process.argv.length < 3 ? fallback : process.argv[2]).trim()
+    return city.length > 0 ? city : fallback
+}
+
+async function getWeatherInfoOf(city) {
+    const { data } = await axios.get(`https://goweather.herokuapp.com/weather/${city}`)
+    const forecast = [{ day: '0', temperature: data.temperature, wind: data.wind }, ...data.forecast]
+
+    return forecast.map((f) => ({ day: parseInt(f.day), temp: f.temperature, wind: f.wind }))
+}
+
+async function main() {
+    const city = getCityArg()
+    const forecast = await getWeatherInfoOf(city)
+
+    console.log(`Weather forecast for ${capitalize(city)} city\n`)
+    for (const weather of forecast) {
+        console.log(` | ${getDate(weather.day)} ~ ${weather.temp}, ${weather.wind}`)
+    }
+}
+
+main().catch((e) => console.error(e))
